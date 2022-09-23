9946e26bf6fc38b68a4b33ec4738bf38223bff3c
Nikitha2005 <84096712+Nikitha2005@users.noreply.github.com>
2022-09-23 12:18:50 -0600

    //

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hi");
+// Arrays Visualizer
+
+// HTML varibales
+let containerEl = document.getElementById("container");
+
+// Global variables
+let myData = []; 
+for(let n =1; n <= 50; n++) {
+    myData.push(Math.random() * 600); 
+}
+
+// Display Data
+drawArray();
+
+function drawArray() {
+    let outputStr = "";
+    for(let i = 0; i < myData.length; i++) {
+        outputStr += `<div style="height:${myData[i]}px"></div>`;
+    }
+    containerEl.innerHTML = outputStr;
+}
+
+// Key Event Stuff
+document.addEventListener("keydown", keydownHandler);
+
+function keydownHandler
